564870bae6eb4f7e5fd22eba2c8ee4e528330825
BulbaSprout <pasabozenko3@gmail.com>
2026-04-21 23:28:57 +0300

    it kinda works but doesnt play music

--- src/main/java/io/github/turbowarp_smp/discs/event/ModEvents.java
+++ src/main/java/io/github/turbowarp_smp/discs/event/ModEvents.java
@@ -8,14 +8,15 @@ import net.minecraft.world.entity.npc.VillagerTrades;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.trading.ItemCost;
 import net.minecraft.world.item.trading.MerchantOffer;
-import net.minecraftforge.event.village.VillagerTradesEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.village.VillagerTradesEvent;
 
 import java.util.List;
 
-@Mod.EventBusSubscriber(modid = Discs.MODID)
+@EventBusSubscriber(modid = Discs.MODID)
 public class ModEvents {
   @SubscribeEvent
   public static void addCustomTrades(VillagerTradesEvent event) {
@@ -37,87 +38,87 @@ public class ModEvents {
 
       // Level 1
       trades.get(1).add((pTrader, pRandom) -> new MerchantOffer(
-          new ItemStack(Items.EMERALD, 4),
+          new ItemCost(Items.EMERALD, 4),
           new ItemStack(Items.NOTE_BLOCK, 1),
           16, 8, 0.02f));
       trades.get(1).add((pTrader, pRandom) -> new MerchantOffer(
-          new ItemStack(Items.EMERALD, 2),
+          new ItemCost(Items.EMERALD, 2),
           new ItemStack(Items.MUSIC_DISC_CAT, 1),
           16, 8, 0.02f));
       trades.get(1).add((pTrader, pRandom) -> new MerchantOffer(
-          new ItemStack(Items.EMERALD, 2),
+          new ItemCost(Items.EMERALD, 2),
           new ItemStack(Items.MUSIC_DISC_BLOCKS, 1),
           16, 8, 0.02f));
       trades.get(1).add((pTrader, pRandom) -> new MerchantOffer(
-          new ItemStack(Items.EMERALD, 4),
+          new ItemCost(Items.EMERALD, 4),
           new ItemStack(Items.MUSIC_DISC_CHIRP, 1),
           16, 8, 0.02f));
       trades.get(1).add((pTrader, pRandom) -> new MerchantOffer(
-          new ItemStack(Items.EMERALD, 2),
+          new ItemCost(Items.EMERALD, 2),
           new ItemStack(Items.MUSIC_DISC_FAR, 1),
           16, 8, 0.02f));
       trades.get(1).add((pTrader, pRandom) -> new MerchantOffer(
-          new ItemStack(Items.EMERALD, 4),
+          new ItemCost(Items.EMERALD, 4),
           new ItemStack(Items.MUSIC_DISC_MALL, 1),
           16, 8, 0.02f));
 
       // Level 2
       trades.get(2).add((pTrader, pRandom) -> new MerchantOffer(
-          new ItemStack(Items.EMERALD, 8),
+          new ItemCost(Items.EMERALD, 8),
           new ItemStack(Items.MUSIC_DISC_MELLOHI, 1),
           16, 8, 0.02f));
       trades.get(2).add((pTrader, pRandom) -> new MerchantOffer(
-          new ItemStack(Items.EMERALD, 16),
+          new ItemCost(Items.EMERALD, 16),
           new ItemStack(Items.MUSIC_DISC_STAL, 1),
           16, 8, 0.02f));
       trades.get(2).add((pTrader, pRandom) -> new MerchantOffer(
-          new ItemStack(Items.EMERALD, 16),
+          new ItemCost(Items.EMERALD, 16),
           new ItemStack(Items.MUSIC_DISC_STRAD, 1),
           16, 8, 0.02f));
 
       // Level 3
       trades.get(3).add((pTrader, pRandom) -> new MerchantOffer(
-          new ItemStack(Items.MUSIC_DISC_WARD, 1),
+          new ItemCost(Items.MUSIC_DISC_WARD, 1),
           new ItemStack(Items.EMERALD, 8),
           16, 8, 0.02f));
       trades.get(3).add((pTrader, pRandom) -> new MerchantOffer(
-          new ItemStack(Items.MUSIC_DISC_11, 1),
+          new ItemCost(Items.MUSIC_DISC_11, 1),
           new ItemStack(Items.DISC_FRAGMENT_5, 1),
           16, 8, 0.02f));
       trades.get(3).add((pTrader, pRandom) -> new MerchantOffer(
-          new ItemStack(Items.EMERALD, 16),
+          new ItemCost(Items.EMERALD, 16),
           new ItemStack(Items.MUSIC_DISC_WAIT, 1),
           16, 8, 0.02f));
 
       // Level 4
       trades.get(4).add((pTrader, pRandom) -> new MerchantOffer(
-          new ItemStack(Items.EMERALD, 12),
+          new ItemCost(Items.EMERALD, 12),
           new ItemStack(Items.JUKEBOX, 1),
           16, 8, 0.02f));
       trades.get(4).add((pTrader, pRandom) -> new MerchantOffer(
-          new ItemStack(Items.MUSIC_DISC_RELIC, 1),
+          new ItemCost(Items.MUSIC_DISC_RELIC, 1),
           new ItemStack(potterySherdItems[pRandom.nextInt(potterySherdItems.length)], 4),
           16, 8, 0.02f));
       trades.get(4).add((pTrader, pRandom) -> new MerchantOffer(
-          new ItemStack(Items.MUSIC_DISC_OTHERSIDE, 1),
+          new ItemCost(Items.MUSIC_DISC_OTHERSIDE, 1),
           new ItemStack(Items.EMERALD, 24),
           16, 8, 0.02f));
 
       // Level 5
       trades.get(5).add((pTrader, pRandom) -> new MerchantOffer(
-          new ItemStack(Items.EMERALD, 32),
+          new ItemCost(Items.EMERALD, 32),
           new ItemStack(ModItems.MUSIC_DISC_NEVER_GONNA_GIVE_YOU_UP.get(), 1),
           16, 8, 0.02f));
       trades.get(5).add((pTrader, pRandom) -> new MerchantOffer(
-          new ItemStack(ModItems.MUSIC_DISC_WITHER_STORM_THEME.get(), 1),
+          new ItemCost(ModItems.MUSIC_DISC_WITHER_STORM_THEME.get(), 1),
           new ItemStack(Items.NETHER_STAR, 1),
           16, 8, 0.02f));
       trades.get(5).add((pTrader, pRandom) -> new MerchantOffer(
-          new ItemStack(Items.MUSIC_DISC_PIGSTEP, 1),
+          new ItemCost(Items.MUSIC_DISC_PIGSTEP, 1),
           new ItemStack(Items.NETHERITE_UPGRADE_SMITHING_TEMPLATE, 1),
           16, 8, 0.02f));
       trades.get(5).add((pTrader, pRandom) -> new MerchantOffer(
-          new ItemStack(Items.ENCHANTED_GOLDEN_APPLE, 1),
+          new ItemCost(Items.ENCHANTED_GOLDEN_APPLE, 1),
           new ItemStack(ModItems.MUSIC_DISC_BAD_APPLE.get(), 1),
           16, 8, 0.02f));
     }
